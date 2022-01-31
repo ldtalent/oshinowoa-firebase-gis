c3e8e27cccd5b67bd2b0202b780a586ff48f7094
Abdulazeez <abdulazeezoshinowo2@gmail.com>
2022-01-31 09:14:24 +0100

    query geohash to firestore

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -7,8 +8,6 @@ import 'package:geolocator/geolocator.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-// import 'package:oshinowoa_firebase_gis/models/model.dart';
-// import 'package:syncfusion_flutter_maps/maps.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -19,7 +18,9 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Geolocator location = Geolocator();
-  static const LatLng center = LatLng(-33.86711, 151.1947171);
+
+  // Position initialPosition = Geolocator.getCurrentPosition();
+  // static const LatLng center = LatLng(initialPosition.latitude, initialPosition.lo);
 
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   Geoflutterfire geo = Geoflutterfire();
@@ -30,8 +31,14 @@ class _HomeScreenState extends State<HomeScreen> {
   int _markerIdCounter = 1;
   LatLng? markerPosition;
 
+  BehaviorSubject<double> radius = BehaviorSubject.seeded(100.0);
+
+  Stream<dynamic>? query;
+
+  StreamSubscription? subscription;
 
   void _onMapCreated(GoogleMapController controller) {
+    _startQuery();
     setState(() {
       mapController = controller;
     });
@@ -52,14 +59,14 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             onMapCreated: _onMapCreated,
             myLocationEnabled: true,
-            myLocationButtonEnabled: false,
+            myLocationButtonEnabled: true,
             mapType: MapType.hybrid,
             compassEnabled: true,
             markers: Set<Marker>.of(markers.values),
             // trafficEnabled: true,
           ),
           Positioned(
-            bottom: 115,
+            bottom: 80,
             right: 15,
             child: ClipRRect(
               borderRadius: BorderRadius.circular(
@@ -73,13 +80,28 @@ class _HomeScreenState extends State<HomeScreen> {
                   padding: const EdgeInsets.all(
                     3.0,
                   ),
-                  onPressed: _animateToUser,
+                  onPressed: _addGeoPoint,
                   icon: const Icon(
                     Icons.pin_drop,
                     color: Colors.white,
                   ),
                 ),
               ),
+            ),
+          ),
+          Positioned(
+            bottom: 70.0,
+            left: 15.0,
+            child: Slider(
+              inactiveColor: Colors.grey,
+              activeColor: Colors.greenAccent,
+              thumbColor: Colors.greenAccent,
+              min: 100.0,
+              max: 500.0,
+              divisions: 4,
+              value: radius.value,
+              label: 'Radius ${radius.value}km',
+              onChanged: _updateQuery,
             ),
           ),
         ],
@@ -102,27 +124,53 @@ class _HomeScreenState extends State<HomeScreen> {
     return store;
   }
 
-  void _addMarker() {
-    final int markerCount = markers.length;
-    if (markerCount == 12) {
-      return;
-    }
-    final String markerIdVal = 'marker_id_$_markerIdCounter';
-    _markerIdCounter++;
-    final MarkerId markerId = MarkerId(markerIdVal);
+  // void _addMarker() {
+  //   final int markerCount = markers.length;
+  //   if (markerCount == 12) {
+  //     return;
+  //   }
+  //   final String markerIdVal = 'marker_id_$_markerIdCounter';
+  //   _markerIdCounter++;
+  //   final MarkerId markerId = MarkerId(markerIdVal);
 
-    final Marker marker = Marker(
-      markerId: markerId,
-      position: LatLng(
-        center.latitude + sin(_markerIdCounter * pi / 6.0) / 20.0,
-        center.longitude + cos(_markerIdCounter * pi / 6.0) / 20.0,
-      ),
-      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
-      onTap: () => _onMarkerTapped(markerId),
-    );
+  //   final Marker marker = Marker(
+  //     markerId: markerId,
+  //     position: mapController.animateCamera(cameraUpdate),
+  //     infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+  //     onTap: () => _onMarkerTapped(markerId),
+  //   );
 
-    setState(() {
-      markers[markerId] = marker;
+  //   setState(() {
+  //     markers[markerId] = marker;
+  //   });
+  // }
+
+  void _updateMarkers(List<DocumentSnapshot> documentList) async {
+    Position loc = await Geolocator.getCurrentPosition();
+    // print(documentList);
+    mapController?.dispose();
+    documentList.forEach((DocumentSnapshot document) {
+      GeoPoint pos = document.get('position')['geopoint'];
+      double distance = document.get('distance');
+
+      GeoFirePoint center = geo.point(latitude: loc.latitude, longitude: loc.longitude);
+
+      final String markerIdVal = 'marker_id_$_markerIdCounter';
+      _markerIdCounter++;
+      final MarkerId markerId = MarkerId(markerIdVal);
+      var marker = Marker(
+        markerId: markerId,
+        position: LatLng(
+          center.latitude + sin(_markerIdCounter * pi / 6.0) / 20.0,
+          center.longitude + cos(_markerIdCounter * pi / 6.0) / 20.0,
+        ),
+        infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+        onTap: () => _onMarkerTapped(markerId),
+      );
+
+      setState(() {
+        markers[markerId] = marker;
+      });
     });
   }
 
@@ -164,5 +212,46 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
     );
+  }
+
+  _startQuery() async {
+    // Get users location
+    Position pos = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.low,
+    );
+    double lat = pos.latitude;
+    double lng = pos.longitude;
+
+    // Make a referece to firestore
+    var ref = firestore.collection('locations');
+    GeoFirePoint center = geo.point(latitude: lat, longitude: lng);
+
+    // subscribe to query
+    subscription = radius.switchMap((rad) {
+      return geo.collection(collectionRef: ref).within(
+          center: center, radius: rad, field: 'position', strictMode: true);
+    }).listen(_updateMarkers);
+  }
+
+  _updateQuery(value) {
+    final zoomMap = {
+      100.0: 12.0,
+      200.0: 10.0,
+      300.0: 7.0,
+      400.0: 6.0,
+      500.0: 5.0
+    };
+    final zoom = zoomMap[value];
+    mapController?.moveCamera(CameraUpdate.zoomTo(zoom!));
+
+    setState(() {
+      radius.add(value);
+    });
+  }
+
+  @override
+  dispose() {
+    subscription?.cancel();
+    super.dispose();
   }
 }
